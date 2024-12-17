2e7b19e89080402c93f1193d9918bd62bbd3b9b4
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-12-17 13:43:41 +0530

    mumbai

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,4 @@
-export const runtime = 'edge'; 
-export const preferredRegion = ['iad1'];
+export const preferredRegion = ['bom1'];
 export const dynamic = 'force-dynamic'; // no caching
  
 export function GET() {
